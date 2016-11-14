4e0f652dfeeece2674a3bf16c4aedf35186e5adf
tobias <tobias.fischer2104@web.de>
2016-11-14 17:49:35 +0100

    Fully parametrized for benchmarking

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include "renderer.h"
 #include <iostream>
 #include <limits>
+#include <iomanip>
+
 int main(int argc, char * argv[])
 {
 	int steps=1000000;
 	int numPoints=12;
+	double movingDistance = 0.01;
 #ifdef sphere
 	double radiusIncrement=1./(double)steps;
 #endif
@@ -13,35 +16,37 @@ int main(int argc, char * argv[])
 	double radiusIncrement=3./(double)steps;
 #endif
 
-	std::cout << "Arguments are:" << std::endl;
+	//std::cout << "Arguments are:" << std::endl;
 	
 	for(int i=0; i<argc; i++){
-		std::cout << i << " " << argv[i] << std::endl;
+		//std::cout << i << " " << argv[i] << std::endl;
 	}
 
 	for(int i=0; i<argc; i++){
 		if(i==1) steps = atoi(argv[i]);
 		if(i==2) numPoints = atoi(argv[i]);
 		if(i==3) radiusIncrement = atof(argv[i]);
+		if(i==4) movingDistance = atof(argv[i]);
 	}
 
-	std::cout << "steps: " << steps << std::endl;
-	std::cout << "points: " << numPoints << std::endl;
-	std::cout << "radInc: " << radiusIncrement << std::endl;
+	//std::cout << "steps: " << steps << std::endl;
+	//std::cout << "points: " << numPoints << std::endl;
+	//std::cout << "radInc: " << radiusIncrement << std::endl;
 
-	std::cout << "deleting data/*" << std::endl;
-	system("rm data/*");
+	//std::cout << "deleting data/*" << std::endl;
+	//system("rm data/*");
 
 	Simulator simulator(numPoints,radiusIncrement);
                     //2147483647
 	for(int i=0; i<steps; i++){
-		if(i%(steps/1000)==0) simulator.saveCoordsToFileOpengl(i/(steps/1000));
+		if(i%(steps/1000)==0) std::cout << i << " " << std::fixed << std::setprecision(7) << 2*simulator.radius << std::endl;
+			//simulator.saveCoordsToFileOpengl(i/(steps/1000));
 
-		simulator.movePoint(0.01);
+		simulator.movePoint(movingDistance);
 		simulator.radius += radiusIncrement;
 		if(simulator.hasCollision()) simulator.radius -= radiusIncrement;
 	}
 
-	std::cout << "Final distance: " << 2*simulator.radius << std::endl;
+	
 	return 0;
 }
